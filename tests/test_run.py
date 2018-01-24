3c7d1bce5e91e915ca86446fe2be973253a456b2
Nicholas Bishop <nicholasbishop@gmail.com>
2018-01-24 10:29:27 -0500

    Add capture test

--- tests/test_run.py
+++ tests/test_run.py
@@ -7,3 +7,7 @@ class TestRun(unittest.TestCase):
     def test_fail(self):
         with self.assertRaises(subprocess.CalledProcessError):
             simplecmd.run('python', '-c', 'exit(1)')
+
+    def test_capture(self):
+        output = simplecmd.run('python', '-c', 'print("hello")', capture=True)
+        self.assertEqual(output.decode('utf-8'), 'hello\n')
